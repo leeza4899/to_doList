f404d749e8a36103336712ac035b160ccdf8330e
leeza4899 <gargleeza4899@gmail.com>
2020-04-25 00:58:17 +0530

    jquery

---
+++ script.js
@@ -0,0 +1,29 @@
+// check off specifed todos by clicking
+$("ul").on("click", "li", function(){
+	$(this).toggleClass("completed");
+});
+
+// click on X to delete to do
+
+$("ul").on("click", "span", function(event){
+	$(this).parent().fadeOut(500,function()
+		{
+		$(this).remove();	
+		});
+	event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event)
+{
+	if(event.which === 13)
+	{
+		var toDotext = $(this).val();
+		$(this).val("");
+		$("ul").append("<li><span><i class='fa fa-trash'></i></span>" + " "+ toDotext + "</li>" )
+	}
+});
+
+
+$(".fa-plus").click(function(){
+	$("input[type = 'text']").fadeToggle()
+})
